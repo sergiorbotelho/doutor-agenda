8646d8694fc1994cfd86233eedd684a2640f36fb
Sergio Botelho <sergiorbotelho@gmail.com>
2025-05-31 11:39:17 -0300

    feat: Implement session management and redirection in authentication and dashboard pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,24 @@
-const DashboardPage = () => {
-  return <div>Dashboard Page</div>;
+
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
+const DashboardPage = async() => {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  });
+  if (!session?.user) {
+   redirect("/authentication");
+  }
+  return <div>
+    <h1>{session?.user?.name}</h1>
+    <h1>{session?.user?.email}</h1>
+    <SignOutButton/>
+   
+  </div>;
 };
 
 export default DashboardPage;
